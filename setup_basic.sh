cba00c9389229d1fec19b9e012da7a98183acf7f
pnagaev <pnagaev@yandex.ru>
2023-10-18 13:25:54 +0300

    Update setup_basic.sh

--- setup_basic.sh
+++ setup_basic.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Run bash <(curl -s https://raw.githubusercontent.com/pnagaev/linux/main/setup_basic.sh)
+#     wget -q -O - https://raw.githubusercontent.com/pnagaev/linux/main/setup_basic.sh | bash
+
 
 echo "Hello World"
