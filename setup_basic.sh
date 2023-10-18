e899bb6d14377f95c60ae9336fd418fe2d7b6f93
pnagaev <pnagaev@yandex.ru>
2023-10-18 13:58:58 +0300

    Update setup_basic.sh

--- setup_basic.sh
+++ setup_basic.sh
@@ -5,13 +5,28 @@
 
 MyOS=$(cat /etc/os-release | awk -F '=' '/^NAME/{print $2}' | awk '{print $1}' | tr -d '"')
 
+NC='\033[0m' # No Color
+# High Intensity
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+
 echo "OS: $MyOS "
+printf "I ${IRED}love${NC} Stack Overflow\n"
 
 if [[ "$MyOS" == @(Debian|RedHat|Astra) ]];then
+  
   apt -y update && apt -y upgrade
+echo "Installing software..."
   apt -y install vim telnet sudo wget ntpdate ntp mc htop iftop tzdata net-tools git curl man bash-completion dnsutils whois
 fi
 if [[ "$MyOS" == @(Centos) ]];then
+  echo -e "Updating system..."
   yum -y update && yum -y upgrade
+  echo -e "Installing software..."
   yum -y install vim telnet sudo wget ntpdate ntp mc htop iftop tzdata net-tools git curl man bash-completion dnsutils whois
 fi
