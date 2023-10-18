d41fc1783205d44656391060fd4a11809763cd50
pnagaev <pnagaev@yandex.ru>
2023-10-18 13:42:50 +0300

    Update setup_basic.sh

--- setup_basic.sh
+++ setup_basic.sh
@@ -6,3 +6,10 @@
 MyOS=$(cat /etc/os-release | awk -F '=' '/^NAME/{print $2}' | awk '{print $1}' | tr -d '"')
 
 echo "OS: $MyOS "
+
+if [ "$MyOS" =~ ^("Debian|RedHat|Astra") ];then
+  apt -y update && apt -y upgrade
+fi
+if [ "$MyOS" =~ ^("Centos") ];then
+  yum -y update && yum -y upgrade
+fi
